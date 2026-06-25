c59c3d45e5224c6fb5976e7bfd9b851f8a61bb9d
fnCodeee <faizna2213@gmail.com>
2026-06-25 07:06:13 +0700

    add(auth): login & register validate

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -2,38 +2,87 @@
 import Image from "next/image";
 import logoApp from "@/assets/images/logos/logo.png";
 import illus from "@/assets/images/ilustrasi/auth-ilustrasion.png";
-import Button from '@/components/ui/Button';
-import Input from '@/components/ui/Input';
-import PasswordInput from '@/components/ui/PasswordInput';
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
+import PasswordInput from "@/components/ui/PasswordInput";
 import React, { useState } from "react";
+import { ROUTES } from "@/constants/routes.js";
+import { useLogin } from "@/hooks/useAuth.js";
+import { useRouter } from "next/navigation.js";
 
 const Login = () => {
-    const [password, setPassword] = useState("");
+  const [data, setData] = useState({});
+  const { login, isLoading, error } = useLogin();
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await login(data);
+  };
 
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
     <section>
-        <link href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css" rel="stylesheet"></link>
-        <link href="https://cdn.boxicons.com/3.0.8/fonts/filled/boxicons-filled.min.css" rel="stylesheet"></link>
-        <link href="https://cdn.boxicons.com/3.0.8/fonts/brands/boxicons-brands.min.css" rel="stylesheet"></link>
-        <div className="flex items-center w-full h-screen justify-between px-16">
-            <Image src={illus} alt="Auth ilustration" className="ml-36"/>
-            <div className="flex flex-col gap-6 justify-center h-full w-full max-w-141.5 items-center">
-                <Image src={logoApp} alt="Astel logo" />
-                <h1 className="text-primary text-center font-medium max-w-70 text-6xl">Welcome Back</h1>
-                <form className="w-full px-35" action="">
-                    <div className="flex flex-col gap-3 w-full">
-                        <Input type="text" placeholder="Username" />
-                        <PasswordInput placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-                    </div>
-                    <Button type="submit" onClick={""} className="mt-6">
-                        Login
-                    </Button>
-                </form>
-                <p className=" text-sm mt-3">Don't have an account? <a href="/auth/register" className="text-primary">Register</a></p>
+      <link
+        href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://cdn.boxicons.com/3.0.8/fonts/filled/boxicons-filled.min.css"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://cdn.boxicons.com/3.0.8/fonts/brands/boxicons-brands.min.css"
+        rel="stylesheet"
+      ></link>
+      <div className="flex items-center w-full h-screen justify-between px-16">
+        <Image src={illus} alt="Auth ilustration" className="ml-36" />
+        <div className="flex flex-col gap-6 justify-center h-full w-full max-w-141.5 items-center">
+          <Image src={logoApp} alt="Astel logo" />
+          <h1 className="text-primary text-center font-medium max-w-70 text-6xl">
+            Welcome Back
+          </h1>
+          <form className="w-full px-35" onSubmit={handleSubmit} action="">
+            {error && <p className="text-red-500">{error}</p>}
+            <div className="flex flex-col gap-3 w-full">
+              <Input
+                type="text"
+                placeholder="Username"
+                name="username"
+                onChange={handleChange}
+              />
+              <PasswordInput
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
             </div>
+
+            <Button
+              type="submit"
+              className={
+                isLoading ? "opacity-50 cursor-not-allowed mt-6" : "mt-6"
+              }
+              disabled={isLoading}
+            >
+              {isLoading ? "Tunggu bentar nyed...!" : "Login"}
+            </Button>
+          </form>
+          <p className=" text-sm mt-3">
+            Don't have an account?{" "}
+            <a href={ROUTES.REGISTER} className="text-primary">
+              Register
+            </a>
+          </p>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Login;
--- src/app/auth/register/page.jsx
+++ src/app/auth/register/page.jsx
@@ -2,39 +2,97 @@
 import Image from "next/image";
 import logoApp from "@/assets/images/logos/logo.png";
 import illus from "@/assets/images/ilustrasi/auth-ilustrasion.png";
-import Button from '@/components/ui/Button';
-import Input from '@/components/ui/Input';
-import PasswordInput from '@/components/ui/PasswordInput';
+import Button from "@/components/ui/Button";
+import Input from "@/components/ui/Input";
+import PasswordInput from "@/components/ui/PasswordInput";
 import React, { useState } from "react";
+import { useRouter } from "next/navigation.js";
+import { ROUTES } from "@/constants/routes.js";
+import { useRegister } from "@/hooks/useAuth.js";
 
 const Register = () => {
-    const [password, setPassword] = useState("");
-    
+  const [data, setData] = useState({});
+  const { register, isLoading, error } = useRegister();
+  const router = useRouter();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const result = await register(data);
+    if (result.data.success) {
+      router.push(ROUTES.LOGIN);
+    }
+    console.log(data);
+  };
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <section>
-        <link href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css" rel="stylesheet"></link>
-        <link href="https://cdn.boxicons.com/3.0.8/fonts/filled/boxicons-filled.min.css" rel="stylesheet"></link>
-        <link href="https://cdn.boxicons.com/3.0.8/fonts/brands/boxicons-brands.min.css" rel="stylesheet"></link>
-        <div className="flex items-center w-full h-screen justify-between px-16">
-            <Image src={illus} alt="Auth ilustration" className="ml-36"/>
-            <div className="flex flex-col gap-6 justify-center h-full w-full max-w-141.5 items-center">
-                <Image src={logoApp} alt="Astel logo" />
-                <h1 className="text-primary text-center font-medium max-w-70 text-6xl">Create account</h1>
-                <form className="w-full px-35" action="">
-                    <div className="flex flex-col gap-3 w-full">
-                        <Input type="email" placeholder="Email" />
-                        <Input type="text" placeholder="Username" />
-                        <PasswordInput placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
-                    </div>
-                    <Button type="submit" onClick={""} className="mt-6">
-                        Create account
-                    </Button>
-                </form>
-                <p className=" text-sm mt-3">Already have an account? <a href="/auth/login" className="text-primary">Login</a></p>
+      <link
+        href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://cdn.boxicons.com/3.0.8/fonts/filled/boxicons-filled.min.css"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://cdn.boxicons.com/3.0.8/fonts/brands/boxicons-brands.min.css"
+        rel="stylesheet"
+      ></link>
+      <div className="flex items-center w-full h-screen justify-between px-16">
+        <Image src={illus} alt="Auth ilustration" className="ml-36" />
+        <div className="flex flex-col gap-6 justify-center h-full w-full max-w-141.5 items-center">
+          <Image src={logoApp} alt="Astel logo" />
+          <h1 className="text-primary text-center font-medium max-w-70 text-6xl">
+            Create account
+          </h1>
+          <form className="w-full px-35" action="" onSubmit={handleSubmit}>
+            {error ? <p className="text-sm text-red-500">{error}</p> : null}
+            <div className="flex flex-col gap-3 w-full">
+              <Input
+                name="email"
+                type="email"
+                placeholder="Email"
+                onChange={handleChange}
+              />
+              <Input
+                name="username"
+                type="text"
+                placeholder="Username"
+                onChange={handleChange}
+              />
+              <PasswordInput
+                name="password"
+                placeholder="Password"
+                onChange={handleChange}
+                required
+              />
             </div>
+            <Button
+              type="submit"
+              className={
+                isLoading ? "opacity-50 cursor-not-allowed mt-6" : "mt-6"
+              }
+              disabled={isLoading}
+            >
+              {isLoading ? "Loading..." : "Register"}
+            </Button>
+          </form>
+          <p className=" text-sm mt-3">
+            Already have an account?{" "}
+            <a href={ROUTES.LOGIN} className="text-primary">
+              Login
+            </a>
+          </p>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
 export default Register;
