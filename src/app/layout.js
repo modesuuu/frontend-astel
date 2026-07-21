f9f57ee333e0ef2894ca1732816aef930ae2bd12
fnCodeee <faizna2213@gmail.com>
2026-07-21 18:41:22 +0700

    browser mobile icon

--- src/app/layout.js
+++ src/app/layout.js
@@ -43,6 +43,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${customFont.variable} h-full antialiased`}>
       <head>
+        <link rel="shortcut icon" href="/favicon.ico" />
         <title>ASTEL</title>
         <link
           href="https://cdn.boxicons.com/3.0.8/fonts/basic/boxicons.min.css"
